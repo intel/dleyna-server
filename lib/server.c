f492c534f1bd58e52fb7c2c6caeaf8cdc16dfe0f
Regis Merlino <regis.merlino@intel.com>
2013-02-26 17:43:47 +0100

    [Fixup] To be squashed - change cleanup order
    Signed-off-by: Regis Merlino <regis.merlino@intel.com>

--- lib/server.c
+++ lib/server.c
@@ -1151,14 +1151,6 @@ static void prv_control_point_initialize(const dleyna_connector_t *connector,
 static void prv_control_point_finalize(void)
 {
 	dls_upnp_unsubscribe(g_context.upnp);
-}
-
-static void prv_control_point_free(void)
-{
-	dls_upnp_delete(g_context.upnp);
-
-	if (g_context.watchers)
-		g_hash_table_unref(g_context.watchers);
 
 	if (g_context.connection) {
 		if (g_context.dls_id)
@@ -1166,6 +1158,14 @@ static void prv_control_point_free(void)
 							g_context.connection,
 							g_context.dls_id);
 	}
+
+	if (g_context.watchers)
+		g_hash_table_unref(g_context.watchers);
+}
+
+static void prv_control_point_free(void)
+{
+	dls_upnp_delete(g_context.upnp);
 }
 
 static const gchar *prv_control_point_server_name(void)
