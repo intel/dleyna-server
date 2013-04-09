f9be2979c5207bd10213f2062f7157a4ac52113c
Sébastien Bianti <sebastien.bianti@linux.intel.com>
2013-04-09 15:15:12 +0200

    [Bug] Add a Rescan method to Manager interface
    - Resolves bug #4
    - Make use of new API gupnp_context_manager_rescan_control_points()
    - Documentation updated.
    
    Signed-off-by: Sébastien Bianti <sebastien.bianti@linux.intel.com>

--- libdleyna/server/upnp.c
+++ libdleyna/server/upnp.c
@@ -1196,3 +1196,10 @@ gboolean dls_upnp_device_context_exist(dls_device_t *device,
 on_exit:
 	return found;
 }
+
+void dls_upnp_rescan(dls_upnp_t *upnp)
+{
+	DLEYNA_LOG_DEBUG("re-scanning control points");
+
+	gupnp_context_manager_rescan_control_points(upnp->context_manager);
+}
