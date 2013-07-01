e56b7dde57b165a6befb1079925ff7eba0134943
Ludovic Ferrandis <ludovic.ferrandis@intel.com>
2013-07-01 15:08:08 +0200

    [Klockwork] NULL pointer may be dereferenced. (#26)
    Fix issue #26: ip_address may be NULL and may be dereferenced.
    
    Signed-off-by: Ludovic Ferrandis <ludovic.ferrandis@intel.com>

--- libdleyna/server/upnp.c
+++ libdleyna/server/upnp.c
@@ -163,11 +163,12 @@ static void prv_server_available_cb(GUPnPControlPoint *cp,
 	prv_device_new_ct_t *priv_t;
 
 	udn = gupnp_device_info_get_udn((GUPnPDeviceInfo *)proxy);
-	if (!udn)
-		goto on_error;
 
 	ip_address = gupnp_context_get_host_ip(
 		gupnp_control_point_get_context(cp));
+
+	if (!udn || !ip_address)
+		goto on_error;
 
 	DLEYNA_LOG_DEBUG("UDN %s", udn);
 	DLEYNA_LOG_DEBUG("IP Address %s", ip_address);
